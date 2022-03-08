a5efd743d0d3ad4833af7c25d623e72499c597db
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-03-08 17:20:32 +0530

    uc1 of EmployeePayrollService Program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeePayrollServiceADO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Services Using ADO.NET Problem");
+
+            EmployeeRepository repository = new EmployeeRepository();  //Creating a object of EmployeeRepository class.
+
+            // UC1 Ensuring the database connection using the sql connection string
+            repository.DataBaseConnection();
+        }
+    }
+}
